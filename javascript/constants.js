4682fb530fee0271cc915f65d3a3a0120053965c
Rolando Munoz <rolandomunoz55@gmail.com>
2024-03-18 14:27:04 -0700

    vieww button fixed

--- javascript/constants.js
+++ javascript/constants.js
@@ -1,6 +1,6 @@
 //constants.js
 
-export const API_URL = 'https://moviesdatabase.p.rapidapi.com/titles/random?list=top_rated_english_250'; //multiple moview titles
+export const API_URL = 'https://moviesdatabase.p.rapidapi.com/titles/random?list=top_rated_english_250'; //multiple moview titles  //titles/ radonm/
 export const API_MOVIE_ID_URL = 'https://moviesdatabase.p.rapidapi.com/titles/{id}?info=base_info'; //single movie
 export const OPTIONS = {
 	method: 'GET',
@@ -9,4 +9,3 @@ export const OPTIONS = {
 		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
 	}
 };
-
